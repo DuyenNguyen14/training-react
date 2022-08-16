c57968c40435d804741534849ec11f2389234afe
Duyen Nguyen <dnng1410@gmail.com>
2022-08-16 15:09:10 +0700

    reactForm- binding data onto layout & save + get value from local storage

--- src/Pages/ReactForm/ReactForm.jsx
+++ src/Pages/ReactForm/ReactForm.jsx
@@ -3,12 +3,60 @@ import FormProd from './FormProd'
 import TableProduct from './TableProduct'
 
 export default class ReactForm extends Component {
+
+    state = {
+        arrProduct: [
+            { id: '1', name: 'product 1', price: 1000, img: 'https://picsum.photos/id/77/150/150', description: 'product 1 description', type: 'mobile' },
+            { id: '2', name: 'product 2', price: 2000, img: 'https://picsum.photos/id/88/150/150', description: 'product 2 description', type: 'tablet' },
+        ]
+    }
+
+    // hàm xử lý state phải nằm ở component chứa state
+    addProduct = (newProd) => {
+        console.log(newProd);
+        let arrProductUpdate = [...this.state.arrProduct, { ...newProd }]; // rest operator: clone arrProduct cũ và thêm newProd vào
+
+        // setState
+        this.setState({
+            arrProduct: arrProductUpdate
+        }, () => {
+            this.saveToLocalStorage();
+        });
+    };
+
+    delProduct = (idDel) => {
+        console.log(idDel);
+
+        let arrProductUpdate = this.state.arrProduct.filter(prod => prod.id !== idDel);
+
+        // setState
+        this.setState({
+            arrProduct: arrProductUpdate
+        }, () => {
+            this.saveToLocalStorage();
+        });
+    };
+
+    saveToLocalStorage() {
+        let value = JSON.stringify(this.state.arrProduct); // chuyển đổi object thành string
+        localStorage.setItem('arrProduct', value);
+    };
+
+    getLocalStorage() {
+        if (localStorage.getItem('arrProduct')) {
+            let arrProduct = JSON.parse(localStorage.getItem('arrProduct')); // chuyển đổi string từ local storage thành object
+            return arrProduct;
+        };
+        return [];
+    }
+
+
     render() {
         return (
             <div className='container'>
                 <h3>Product managenent</h3>
-                <FormProd />
-                <TableProduct />
+                <FormProd addProduct={this.addProduct} />
+                <TableProduct arrProduct={this.state.arrProduct} delProduct={this.delProduct} />
             </div>
         )
     }
--- src/Pages/ReactForm/TableProduct.jsx
+++ src/Pages/ReactForm/TableProduct.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 
 export default class TableProduct extends Component {
     render() {
+        let { arrProduct } = this.props;
         return (
             <table className="table">
                 <thead>
@@ -16,7 +17,24 @@ export default class TableProduct extends Component {
                     </tr>
                 </thead>
                 <tbody>
-                    
+                    {arrProduct.map((prod, index) => {
+                        return <tr key={index}>
+                            <td>{prod.id}</td>
+                            <td>
+                                <img src={prod.img} alt="..." />
+                            </td>
+                            <td>{prod.name}</td>
+                            <td>{prod.price}</td>
+                            <td>{prod.type}</td>
+                            <td>{prod.description}</td>
+                            <td>
+                                <button className="btn btn-danger" onClick={() => {
+                                    this.props.delProduct(prod.id);
+                                }}>Delete</button>
+                                <button className="btn btn-warning ms-2">Edit</button>
+                            </td>
+                        </tr>
+                    })}
                 </tbody>
             </table>
         )
